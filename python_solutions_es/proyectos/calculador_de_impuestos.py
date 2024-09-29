475b6d386bc332985c2b7def2bab6a1685386bfd
Alan Ccoya <alanccoya@outlook.com>
2024-09-29 20:07:52 -0300

    Add files via upload

---
+++ python_solutions_es/proyectos/calculador_de_impuestos.py
@@ -0,0 +1,15 @@
+#Calculadora de impuestos sobre la renta
+
+ingreso= float(input("Por favor, ingresa tu ingreso anual:"))
+
+if ingreso < 0:
+    print("El ingreso no puede ser negativo.")
+else:
+    if ingreso <= 10000:
+        tasa = 0.10
+    elif ingreso <= 50000:
+        tasa = 0.20
+    else:
+        tasa = 0.30
+    impuesto = ingreso * tasa
+    print(f"Debes pagar ${impuesto:2.f} en impuestos.")
