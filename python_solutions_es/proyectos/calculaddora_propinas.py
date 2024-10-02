2e849b0758af5528c611a847ba1887c67c2f9bd5
Alan Ccoya <alanccoya@outlook.com>
2024-10-02 02:01:43 -0300

    Add files via upload

---
+++ python_solutions_es/proyectos/calculaddora_propinas.py
@@ -0,0 +1,25 @@
+#Calculadora de propinas!
+
+#solicitar el total de la cuenta
+total_cuenta = float(input("Por favor, ingrese el total de la cuenta:"))
+
+#solicitar evalucion del servicio recivido 
+servicio = input("Como fue el servicio? (bueno, regular, malo):").lower()
+
+if servicio == bueno:
+    porcentaje_propina = 0.20
+elif servicio == regular:
+    porcentaje_propina = 0.15
+elif servicio == malo:
+    porcentaje_propina = 0.10
+else:
+    print("Nivel de servicio no válido.")
+    porcentaje_propina = 0
+
+#Calculamos el monto de la propina
+propina = total_cuenta * porcentaje_propina
+#calculamos el total con propina
+total_con_propina =  total_cuenta * propina
+
+print(f"La propina es: {propina:.2f}")
+print(f"El total a pagar con propina es: {total_con_propina:.2f}")
