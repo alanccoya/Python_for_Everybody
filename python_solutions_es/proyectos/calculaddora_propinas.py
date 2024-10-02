ef8e8049f0100be020beab8ff1391063795aa6a8
Alan Ccoya <alanccoya@outlook.com>
2024-10-02 02:27:04 -0300

    Update calculaddora_propinas.py

--- python_solutions_es/proyectos/calculaddora_propinas.py
+++ python_solutions_es/proyectos/calculaddora_propinas.py
@@ -6,11 +6,11 @@ total_cuenta = float(input("Por favor, ingrese el total de la cuenta:"))
 #solicitar evalucion del servicio recivido 
 servicio = input("Como fue el servicio? (bueno, regular, malo):").lower()
 
-if servicio == bueno:
+if servicio == "bueno":
     porcentaje_propina = 0.20
-elif servicio == regular:
+elif servicio == "regular":
     porcentaje_propina = 0.15
-elif servicio == malo:
+elif servicio == "malo":
     porcentaje_propina = 0.10
 else:
     print("Nivel de servicio no válido.")
