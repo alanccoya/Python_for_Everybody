8bc2138043a0ed392e9137e1eb53ebb6b598694b
Alan Ccoya <alanccoya@outlook.com>
2024-10-14 22:46:47 -0300

    Update and rename boolean.py to ex1_v2.py

---
+++ python_solutions_es/capitulo_6/ex1_v2.py
@@ -0,0 +1,5 @@
+# en este codigo usaremos for
+fruta = "maracuya"
+
+for letra in reversed(fruta): # "reversed(fruta)" la funcion toma una secuencia y devuelve un iterador que recorre los elementos en orden inverso.
+    print(letra)
