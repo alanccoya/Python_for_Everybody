32f1cc79ba7c4ec243b855997a46e958dad1b598
Alan Ccoya <alanccoya@outlook.com>
2025-01-19 21:46:13 -0300

    Create ex_7-1.py

---
+++ python_solutions_en/chapter_7/ex_7-1.py
@@ -0,0 +1,12 @@
+@by @alanccoya
+
+fname = input("Enter file name:")
+
+try:
+    fh = open(fname)
+except:
+    print("File cannot be opened:", fname)
+    quit()
+
+for line in fh:
+    print(line.upper().strip())
